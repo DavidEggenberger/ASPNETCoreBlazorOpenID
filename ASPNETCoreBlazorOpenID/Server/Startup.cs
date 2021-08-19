777ca3f098842f071d15bf897770cfc09bc6aa8f
DavidEggenberger <david99.eggenberger@gmail.com>
2021-08-20 01:32:21 +0200

    Remove ASPCore Identity

--- ASPNETCoreBlazorOpenID/Server/Startup.cs
+++ ASPNETCoreBlazorOpenID/Server/Startup.cs
@@ -26,21 +26,6 @@ namespace ASPNETCoreBlazorOpenID.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
-
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -67,7 +52,6 @@ namespace ASPNETCoreBlazorOpenID.Server
 
             app.UseRouting();
 
-            app.UseIdentityServer();
             app.UseAuthentication();
             app.UseAuthorization();
 
